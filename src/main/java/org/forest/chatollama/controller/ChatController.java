722cabd13a3b12f802774eb237ceacaa378c5910
居森林 <1121264910@qq.com>
2025-03-12 17:44:39 +0800

    refactor(chat): 重构聊天控制器并添加验证支持
    - 重构 ChatController 类，使用 POST 请求替代 GET 请求
    - 添加 spring-boot-starter-validation 依赖，支持参数验证
    - 更新 hutool-all版本至 5.8.35
    - 优化代码结构，提高可维护性和扩展性

--- src/main/java/org/forest/chatollama/controller/ChatController.java
+++ src/main/java/org/forest/chatollama/controller/ChatController.java
@@ -1,21 +1,20 @@
 package org.forest.chatollama.controller;
 
 
+import org.forest.chatollama.dto.ChatMessageRequest;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
-@CrossOrigin
 @RestController
 @RequestMapping(value = "/Chat")
+@Validated
 public class ChatController {
 
 
@@ -23,9 +22,9 @@ public class ChatController {
     private OllamaChatModel chatModel;
 
 
-    @RequestMapping(value = "/message", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<ChatResponse> generateStream(@RequestParam(value = "message") String message) {
-        return chatModel.stream(new Prompt(new UserMessage(message))).cache();
+    @PostMapping(value = "/message", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ChatResponse> generateStream(@RequestBody @Validated ChatMessageRequest request) {
+        return chatModel.stream(new Prompt(new UserMessage(request.getMessage()))).cache();
     }
 
 
