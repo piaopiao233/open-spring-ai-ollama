95879ddf22d5707cbeffff666a4b4afcbec7e6c9
居森林 <1121264910@qq.com>
2025-03-13 17:46:28 +0800

    feat(config): 添加数据库配置和 MyBatis 支持

--- src/test/java/org/forest/chatollama/ChatOllamaApplicationTests.java
+++ src/test/java/org/forest/chatollama/ChatOllamaApplicationTests.java
@@ -9,13 +9,11 @@ import java.util.List;
 @SpringBootTest
 class ChatOllamaApplicationTests {
 
-    @Autowired
-    private SysUserMapper sysUserMapper;
+
     @Test
     void contextLoads() {
-        List<SysUser> sysUsers = sysUserMapper.selectList(null);
 
-        System.out.println(sysUsers);
+        System.out.println();
     }
 
 }
