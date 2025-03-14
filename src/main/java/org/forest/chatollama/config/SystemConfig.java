40f01241c13ac9ff8756c9207b3ac18ee07d91b4
居森林 <1121264910@qq.com>
2025-03-14 09:01:49 +0800

    build(dependencies): 更新 MyBatis-Plus 依赖
    - 添加 MyBatis-Plus 拦截器配置
    - 更新 mybatis-plus-spring-boot3-starter 依赖至 3.5.10.1 版本
    - 添加 mybatis-plus-jsqlparser 依赖 3.5.10.1 版本

--- src/main/java/org/forest/chatollama/config/SystemConfig.java
+++ src/main/java/org/forest/chatollama/config/SystemConfig.java
@@ -1,6 +1,11 @@
 package org.forest.chatollama.config;
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -17,5 +22,15 @@ public class SystemConfig implements WebMvcConfigurer {
                 .addPathPatterns("/**");
 
     }
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.POSTGRE_SQL));
+        // 防止误操作全表更新插件
+        interceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
+        return interceptor;
+    }
+
 
 }
