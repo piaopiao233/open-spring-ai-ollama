08054e87c02191c113f93f4de706bd3726df6b66
居森林 <1121264910@qq.com>
2025-03-17 17:02:49 +0800

    build(dependencies): 添加 MyBatis Plus 代码生成器相关依赖- 在 pom.xml 中添加 mybatis-plus-generator 和 freemarker 依赖 - 新增 CodeGenerator 类用于生成代码 - 移除 application.yml 中的 application name 配置

---
+++ src/main/java/org/forest/chatollama/CodeGenerator.java
@@ -0,0 +1,36 @@
+package org.forest.chatollama;
+
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.nio.file.Paths;
+import java.util.Collections;
+
+public class CodeGenerator {
+
+    public static void main(String[] args) {
+        FastAutoGenerator.create("jdbc:postgresql://localhost:5432/postgres", "postgres", "123456")
+                .globalConfig(builder -> builder
+                        .author("居森林")
+                        .outputDir(Paths.get(System.getProperty("user.dir")) + "/src/main/java")
+                        .commentDate("yyyy-MM-dd HH:mm:ss")
+                )
+                .packageConfig(builder -> builder
+                        .parent("org.forest.chatollama")
+                        .entity("model")
+                        .mapper("mapper")
+                        .service("service")
+                        .serviceImpl("service.impl")
+                        .pathInfo(Collections.singletonMap(OutputFile.xml,
+                                System.getProperty("user.dir") + "/src/main/resources/mapper"))  // XML 路径
+                )
+                .strategyConfig(builder -> {
+                            builder.entityBuilder().enableLombok();
+                            builder.addInclude("sys_user").controllerBuilder().enableRestStyle();
+                        }
+                )
+                .templateEngine(new FreemarkerTemplateEngine())
+                .execute();
+    }
+}
