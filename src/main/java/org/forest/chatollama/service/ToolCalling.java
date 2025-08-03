8cb6b3bc0dc82deeed0c6f5a445c89e319c2321e
jusenlin <1121264910@qq.com>
2025-08-03 11:24:43 +0800

    build(dependencies): 更新依赖版本并调整 MariaDB 驱动- 更新 spring-ai-bom依赖至 1.0.1 版本 - 移除 spring-ai-starter-vector-store-mariadb 依赖中的 exclusions配置 - 单独添加 mariadb-java-client 依赖，版本为 3.5.3- 删除重复的 mariadb-java-client 依赖项 - 调整 ToolCalling 类中的 getStudentAttendance 方法参数描述

--- src/main/java/org/forest/chatollama/service/ToolCalling.java
+++ src/main/java/org/forest/chatollama/service/ToolCalling.java
@@ -23,7 +23,7 @@ public class ToolCalling {
 
     @Tool(name = "获取某个学生某天的考勤", description = "获取某个学生某天的考勤")
     public String getStudentAttendance(@ToolParam(required = true, description = "姓名") String name,
-                                       @ToolParam(required = false, description = "日期， 可选，格式 yyyy-MM-dd（默认当天") String date) {
+                                       @ToolParam(required = false, description = "日期， 可选，格式 yyyy-MM-dd（默认为空，今天也传空值") String date) {
         if (StrUtil.isBlank(name)) {
             return "请填写姓名";
         }
